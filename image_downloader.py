fa77d569af4dc084dcd01f7111dce00109e56129
dsouheil <souheil.dehmani@gmail.com>
2016-10-06 12:55:25 +0200

    Updated image_downloader with main function

--- image_downloader.py
+++ image_downloader.py
@@ -2,7 +2,7 @@
 """ Downloades images from URLs in a text file.
 
 the text file is given as the first argument and must contain 
-the an URL in each line. The images are either saved in the 
+an URL in each line. The images are either saved in the 
 current directory or in a given local path.
 """
 
@@ -15,26 +15,25 @@ __maintainer__ = "Souheil Dehmani"
 __email__ = "souheil.dehmani@gmail.com"
 __status__ = "Prototype"
 
-# set to the desired path to save the downloaded images
-local_path = "~/desired/path/blueyonder/images/"
+# set to the desired path to save the downloaded images. Leave empty to save in current directory
+local_path = ""
 
+def download_images():
+    with open(sys.argv[1], 'r') as url_file:
+        success = 0    # counter for successful donwloads 
+        fail = 0       # counter for failed downloads
+        for line in url_file:
+            image_url = line
+            try:
+                urllib.urlretrieve(image_url, local_path + "image_" + str(success) + ".jpg")
+                success += 1
 
-with open(sys.argv[1], 'r') as url_file:
-    success = 0    # counter for successful donwloads 
-    fail = 0       # counter for failed downloads
-    for line in url_file:
-        image_url = line
-        try:
-            # Use this line to save images in the given local path
-            #urllib2.urlretrieve(image_url, local_path + "image_" + str(success) + ".jpg")
+            except IOError as err:
+                print(err)   # something wrong with the given local path or URL
+                fail += 1
 			
-            # This line will save images in the current directory. Comment it out if the previous line is used
-            urllib.urlretrieve(image_url, "image_" + str(success) + ".jpg")
-            success += 1
-
-        except IOError as err:
-            print(err)   # something wrong with the given local path or URL
-            fail += 1
-			
-    print str(success) + " images saved with success"
-    print str(fail) + " failures" 
+        print str(success) + " images saved with success"
+        print str(fail) + " failures" 
+		
+if __name__ == "__main__":
+    download_images()
